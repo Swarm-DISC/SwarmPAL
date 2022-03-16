40be8daa39b8ffcde04dfbb56c9450e04aed97c8
Ashley Smith <ashley.smith@magneticearth.org>
2022-03-16 13:28:34 +0000

    Fix formatting

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,14 +1,17 @@
+from __future__ import annotations
+
 from xarray import Dataset
-from swarmx.io import ViresDataFetcher, MagData
+
+from swarmx.io import MagData, ViresDataFetcher
 
 
 def test_ViresDataFetcher():
     parameters = {
-        'collection': 'SW_OPER_MAGA_LR_1B',
-        'measurements': ['F'],
-        'models': ['IGRF'],
-        'auxiliaries': ['QDLat'],
-        'sampling_step': "PT1M"
+        "collection": "SW_OPER_MAGA_LR_1B",
+        "measurements": ["F"],
+        "models": ["IGRF"],
+        "auxiliaries": ["QDLat"],
+        "sampling_step": "PT1M",
     }
     v = ViresDataFetcher(parameters=parameters)
     start = "2022-01-01T00:00:00"
@@ -18,13 +21,13 @@ def test_ViresDataFetcher():
         _type = type(ds)
         raise TypeError(f"Returned data should be xarray.Dataset, not {_type}")
     expected_vars = [
-        'Spacecraft',
-        'Radius',
-        'Latitude',
-        'Longitude',
-        'F',
-        'F_IGRF',
-        'QDLat',
+        "Spacecraft",
+        "Radius",
+        "Latitude",
+        "Longitude",
+        "F",
+        "F_IGRF",
+        "QDLat",
     ]
     if set(expected_vars) != set(ds.data_vars):
         raise ValueError(f"Erroneous data_vars: {ds.data_vars}")
