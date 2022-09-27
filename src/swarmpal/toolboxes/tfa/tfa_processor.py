5d792545cd478e14dc23df9920d1ef6a01b45a0e
Ashley Smith <ashley.smith@magneticearth.org>
2022-09-27 14:32:50 +0100

    Quick fixes to plot code from Constantinos

--- src/swarmpal/toolboxes/tfa/tfa_processor.py
+++ src/swarmpal/toolboxes/tfa/tfa_processor.py
@@ -9,6 +9,7 @@ import datetime as dt
 from abc import ABC, abstractmethod
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from swarmpal.io import ExternalData
 from swarmpal.toolboxes.tfa import tfalib
@@ -54,14 +55,36 @@ class TfaProcessor:
                 "maglat_lims": [-60, 60],
             }
         )
-        self.t = self.input_data.xarray["Timestamp"].dt.second.copy()
+        self.t = self._time_in_seconds(self.input_data.xarray["Timestamp"])
         self.X = self.input_data.xarray[X_varname].data.copy()
         self.meta = {"General": self.params}
+
+    @staticmethod
+    def _time_in_seconds(timestamps):
+        """Time in seconds from beginning of data"""
+        return (timestamps - timestamps[0]).dt.seconds.data
 
     @property
     def analysis_window(self):
         """list[datetime]: pair of unpadded times"""
         return self.input_data.analysis_window
+
+    @property
+    def analysis_window_indexes(self):
+        """tuple[int]: indexes where analysis starts and ends"""
+        try:
+            # Use precalculated if available
+            idx0, idx1 = self._idx0, self._idx1
+        except AttributeError:
+            # Identify indexes at the given times from analysis_window
+            window_dt64 = np.array(self.analysis_window).astype("datetime64[ns]")
+            timestamps = self.input_data.xarray["Timestamp"].data
+            idx0 = np.argwhere(timestamps == window_dt64[0])
+            idx1 = np.argwhere(timestamps == window_dt64[1])
+            idx0, idx1 = idx0[0, 0], idx1[0, 0]
+            # Store for next usage
+            self._idx0, self._idx1 = idx0, idx1
+        return idx0, idx1
 
     @property
     def t(self):
@@ -85,14 +108,71 @@ class TfaProcessor:
         """Apply a TFA Process"""
         process.apply(self)
 
-    def plot(self):
-        pass
+    def plot(self, data="Series"):
+        if data == 'Series':
+            x = self.X
+        elif data == 'Index':
+            x = self.I
+        elif data=='QD':
+            pass
+        
+        D = self.X.shape[1]
+        
+        plt.figure()
+        for i in range(D):
+            plt.subplot(D,1,i+1)
+            plt.plot(self.t, x[:,i], '-b')
+            plt.xlim(self.analysis_window_indexes)
+            plt.grid(True)
+            # if i == 0:
+            #     plt.title(self.label)
 
     def image(self):
-        pass
+        M, N, D = self.W.shape
+        freqs = 1000/self.s
+        # freq_lims = [freqs[0], freqs[-1]]
+        # yticks = np.hstack((np.arange(1,10), np.arange(10,200,20), 
+        #                    np.arange(200,1000,200), np.arange(1000,10000,1000)))
+        # yticks = np.append(yticks, freq_lims)
+        # yticklabels = ['%.0f'%i for i in yticks]
 
-    def wave_index(self):
-        pass
+        plt.figure()
+        for i in range(D):
+            m = np.max([np.log10(np.min(self.W[::, :, i])), -6])
+            x = np.log10(np.max(self.W[::, :, i]))
+            
+            plt.subplot(D,1,i+1)
+            plt.contourf(self.t, freqs, np.log10(self.W[::, :, i]), 
+                         cmap='jet', levels=np.linspace(m, x, 20), 
+                         extend='min')
+            plt.xlim(self.analysis_window_indexes)
+            # plt.yticks(ticks=yticks, labels=yticklabels)
+            # plt.ylim(freq_lims)
+            plt.ylabel('Freq (mHz)')
+            cbh = plt.colorbar(orientation='vertical')
+            cb_ticks = cbh.get_ticks()
+            cbh.set_ticks(cb_ticks)
+            cbh.set_ticklabels(['%.2f'%i for i in cb_ticks])
+            # if i == 0:
+            #     plt.title(self.label)
+
+    def _evaluate_wave_index(self):
+        N, D = self.X.shape
+        I = np.full((N,D), np.NaN)
+
+        for i in range(D):
+            I[:,i] = np.sum(self.W[:,:,i], axis=0)
+
+        return I
+
+    @property
+    def I(self):
+        """Wave index"""
+        try:
+            return self._I
+        except AttributeError:
+            self._I = self._evaluate_wave_index()
+        return self._I
 
 
 class TFA_Process(ABC):
