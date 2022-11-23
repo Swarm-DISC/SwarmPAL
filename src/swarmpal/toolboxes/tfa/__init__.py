d66f649f6ec18abb1d31c020a579be80476a0e99
Ashley Smith <ashley.smith@magneticearth.org>
2022-11-23 15:11:44 +0000

    Fix init files

--- src/swarmpal/toolboxes/tfa/__init__.py
+++ src/swarmpal/toolboxes/tfa/__init__.py
@@ -1,21 +1,17 @@
 from swarmpal.toolboxes.tfa import tfalib
-from swarmpal.toolboxes.tfa.tfa_processor import (
-    Cadence,
+from swarmpal.toolboxes.tfa.tfa_processor import (  # Cadence,; TfaEfiInputs,; TfaMagInputs,
     Cleaning,
     Filtering,
-    TfaEfiInputs,
-    TfaMagInputs,
     TfaProcessor,
     Wavelet,
 )
 
 __all__ = (
-    "TfaMagInputs",
-    "TfaEfiInputs",
-    "TfaProcessor",
-    "tfalib",
-    "Cadence",
-    "Cleaning",
-    "Filtering",
-    "Wavelet",
+    # Cadence,
+    Cleaning,
+    Filtering,
+    # TfaEfiInputs,
+    # TfaMagInputs,
+    TfaProcessor,
+    Wavelet,
 )
