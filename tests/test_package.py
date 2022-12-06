c7b9a98b116d0207b872536aad57912e02c783d6
Ashley Smith <ashley.smith@magneticearth.org>
2022-12-06 16:21:10 +0000

    Make module test check module docstrings

--- tests/test_package.py
+++ tests/test_package.py
@@ -6,5 +6,9 @@ import swarmpal as m
 def test_version():
     assert m.__version__
 
+
 def test_imports():
-    from swarmpal.toolboxes.tfa import tfalib, tfa_processor
+    from swarmpal.toolboxes.tfa import tfa_processor, tfalib
+
+    for module in (tfa_processor, tfalib):
+        print(module.__doc__)
