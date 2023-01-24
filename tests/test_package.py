e3362563f47c72ab89fa22964f076cd95cf3a0eb
Ashley Smith <ashley.smith@magneticearth.org>
2023-01-24 15:44:13 +0000

    TFA project delivery (#13)
    * Bump pre-commit/action from 2.0.3 to 3.0.0 (#4)
    
    Bumps [pre-commit/action](https://github.com/pre-commit/action) from 2.0.3 to 3.0.0.
    - [Release notes](https://github.com/pre-commit/action/releases)
    - [Commits](https://github.com/pre-commit/action/compare/v2.0.3...v3.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: pre-commit/action
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * test of code delivery for TFA
    
    * Cleanup with pre-commit
    
    * Fix typos caught by pre-commit
    
    * Ignore data file directories in pre-commi
    
    * Revert alteration of data file
    
    * Fix init files
    
    * Remove vscode settings
    
    * Fix init file
    
    * Remove imports from submodules at tfa module
    
    * Include Include new TFA notebooks in docs
    
    * Add basic import test
    
    * Add missing matplotlib dependency
    
    * Tidying up Notebooks
    
    * Fix execution of notebooks with different kernel names
    
    See https://myst-nb.readthedocs.io/en/latest/computation/execute.html#execute-with-a-different-kernel-name
    
    Ideally notebooks will have kernel names scrubbed with pre-commit, but this should fix that in the interim. Might cause problems when building the docs locally.
    
    * Added documentation, removed sampling_time from input parameters and added the possibility for the user to give frequencies instead of scales in wavelet function
    
    * Fix style and spelling
    
    * Make module test check module docstrings
    
    * Replace print with logging.warn
    
    * Add logging config
    
    * Bump version
    
    * Remove Cluster demo for now
    
    * Adjust API display in docs
    
    * Let pytest ignore warnings (from apexpy)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: constantinus.0 <constantinus.0@gmail.com>

--- tests/test_package.py
+++ tests/test_package.py
@@ -5,3 +5,10 @@ import swarmpal as m
 
 def test_version():
     assert m.__version__
+
+
+def test_imports():
+    from swarmpal.toolboxes.tfa import tfa_processor, tfalib
+
+    for module in (tfa_processor, tfalib):
+        print(module.__doc__)
