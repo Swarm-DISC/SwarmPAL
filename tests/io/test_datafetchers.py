68ccd1e13ec6bc18f7ee1d60b2fa54aa1747f096
Ashley Smith <ashley.smith@magneticearth.org>
2023-02-23 15:35:16 +0000

    Fix tests

--- tests/io/test_datafetchers.py
+++ tests/io/test_datafetchers.py
@@ -34,14 +34,14 @@ def test_vires_fetcher():
 @pytest.mark.remote
 def test_hapi_fetcher():
     params = dict(
-        collection="SW_OPER_MAGA_LR_1B",
-        measurements=["F", "B_NEC"],
-        start_time="2016-01-01T00:00:00",
-        end_time="2016-01-01T00:00:10",
+        dataset="SW_OPER_MAGA_LR_1B",
+        parameters="F,B_NEC",
+        start="2016-01-01T00:00:00",
+        stop="2016-01-01T00:00:10",
     )
     hapi_params = dict(
         **params,
-        server_url="https://vires.services/hapi",
+        server="https://vires.services/hapi",
         options=dict(logging=True),
     )
     # TODO: find source of warning (which is filtered out here)
--- tests/io/test_paldata.py
+++ tests/io/test_paldata.py
@@ -5,7 +5,7 @@ import pytest
 from pandas import to_datetime as to_pandas_datetime
 from xarray import Dataset, open_dataset
 
-from swarmpal.io.paldata import PalData, PalDataItem
+from swarmpal.io.paldata import PalDataItem
 
 
 @pytest.mark.remote
@@ -40,14 +40,14 @@ def xarray_data_file(tmp_path):
 @pytest.mark.remote
 def test_paldataitem_hapi():
     params = dict(
-        collection="SW_OPER_MAGA_LR_1B",
-        measurements=["F", "B_NEC"],
-        start_time="2016-01-01T00:00:00",
-        end_time="2016-01-01T00:00:10",
+        dataset="SW_OPER_MAGA_LR_1B",
+        parameters="F,B_NEC",
+        start="2016-01-01T00:00:00",
+        stop="2016-01-01T00:00:10",
     )
     hapi_params = dict(
         **params,
-        server_url="https://vires.services/hapi",
+        server="https://vires.services/hapi",
         options=dict(logging=True),
     )
     item = PalDataItem.from_hapi(**hapi_params)
@@ -66,67 +66,51 @@ def test_paldataitem_manual(xarray_data_file):
     assert isinstance(item.xarray, Dataset)
 
 
-def test_paldata(xarray_data_file):
-    mypal = PalData(
-        PalDataItem.from_file(xarray_data_file),
-        PalDataItem.from_file(xarray_data_file),
-    )
-    mypal.initialise()
-    assert isinstance(mypal[0].xarray, Dataset)
-    assert isinstance(mypal[1].xarray, Dataset)
-    mypal = PalData(
-        one=PalDataItem.from_file(xarray_data_file),
-        two=PalDataItem.from_file(xarray_data_file),
-    )
-    assert isinstance(mypal["one"].xarray, Dataset)
-    assert isinstance(mypal["two"].xarray, Dataset)
-
-
 @pytest.mark.remote
-def test_time_pad_vires_hapi():
+def test_time_pad_vires():
     start_time = datetime(2016, 1, 1, 0, 0, 0)
     end_time = datetime(2016, 1, 1, 0, 0, 10)
     dt0 = timedelta(seconds=3)
     dt1 = timedelta(seconds=5)
-    params = dict(
+    vires_params = dict(
         collection="SW_OPER_MAGA_LR_1B",
         measurements=["F", "B_NEC"],
         start_time=start_time,
         end_time=end_time,
         pad_times=[dt0, dt1],
-    )
-    vires_params = dict(
-        **params,
         server_url="https://vires.services/ows",
         options=dict(asynchronous=False, show_progress=False),
     )
-    mypal = PalData(PalDataItem.from_vires(**vires_params))
+    pdi = PalDataItem.from_vires(**vires_params)
     # Check that the start and end times of data are offset correctly
-    t0, t1 = to_pandas_datetime(mypal[0].xarray["Timestamp"][[0, -1]])
+    t0, t1 = to_pandas_datetime(pdi.xarray["Timestamp"][[0, -1]])
     assert t0 == start_time - dt0
     assert t1 == end_time + dt1 - timedelta(seconds=1)
-    # Check that analysis window matches the given start, end times
-    assert mypal[0].analysis_window[0] == start_time
-    assert mypal[0].analysis_window[1] == end_time
+    # # Check that analysis window matches the given start, end times
+    # add this when finalised. NB. must deserialise the PAL_meta
+    # assert pdi.xarray.attrs["PAL_meta"]["analysis_window"][0] == start_time.isoformat()
+    # assert pdi.xarray.attrs["PAL_meta"]["analysis_window"][1] == end_time.isoformat()
 
+
+@pytest.mark.remote
+def test_time_pad_hapi():
+    start_time = datetime(2016, 1, 1, 0, 0, 0)
+    end_time = datetime(2016, 1, 1, 0, 0, 10)
+    dt0 = timedelta(seconds=3)
+    dt1 = timedelta(seconds=5)
     hapi_params = dict(
-        **params,
-        server_url="https://vires.services/hapi",
+        dataset="SW_OPER_MAGA_LR_1B",
+        parameters="F,B_NEC",
+        start=start_time.isoformat(),
+        stop=end_time.isoformat(),
+        pad_times=[dt0, dt1],
+        server="https://vires.services/hapi",
     )
-    mypal = PalData(PalDataItem.from_hapi(**hapi_params))
+    pdi = PalDataItem.from_hapi(**hapi_params)
     # Check that the start and end times of data are offset correctly
-    t0, t1 = to_pandas_datetime(mypal[0].xarray["Timestamp"][[0, -1]])
+    t0, t1 = to_pandas_datetime(pdi.xarray["Timestamp"][[0, -1]])
     assert t0 == start_time - dt0
     assert t1 == end_time + dt1 - timedelta(seconds=1)
-    # Check that analysis window matches the given start, end times
-    assert mypal[0].analysis_window[0] == start_time
-    assert mypal[0].analysis_window[1] == end_time
-
-
-def test_paldata_magnetic_models(xarray_data_file):
-    mypal = PalData(
-        PalDataItem.from_file(xarray_data_file),
-    )
-    models = mypal[0].magnetic_models
-    assert list(models.keys())[0] == "IGRF"
-    assert list(models.values())[0] == "IGRF(max_degree=13,min_degree=1)"
+    # # Check that analysis window matches the given start, end times
+    # assert pdi.xarray.attrs["PAL_meta"]["analysis_window"][0] == start_time.isoformat()
+    # assert pdi.xarray.attrs["PAL_meta"]["analysis_window"][1] == end_time.isoformat()
