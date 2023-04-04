085224c964f6a75c2cfd1d9622abbf6560b23e53
Ashley Smith <ashley.smith@magneticearth.org>
2023-04-04 17:12:22 +0100

    Add custom time slice plotting; docstrings

--- src/swarmpal/toolboxes/tfa/plotting.py
+++ src/swarmpal/toolboxes/tfa/plotting.py
@@ -15,28 +15,47 @@ def _get_tfa_meta(datatree):
     return pal_processes_meta
 
 
-def _get_active_dataset_window(datatree, meta=None, clip_times=True):
+def _get_active_dataset_window(datatree, meta=None, clip_times=True, tlims=None):
     """Get the dataset, subselected to the analysis window"""
     pal_processes_meta = meta if meta else _get_tfa_meta(datatree)
     tfa_preprocess_meta = pal_processes_meta.get("TFA_Preprocess")
     subtree = datatree[tfa_preprocess_meta.get("dataset")]
-    try:
-        dataset_palmeta = subtree.swarmpal.pal_meta["."]
-        window = dataset_palmeta["analysis_window"]
-        if clip_times:
-            subset_ds = subtree.ds.sel({"TFA_Time": slice(window[0], window[1])})
-        else:
-            subset_ds = subtree.ds
-        return subset_ds
-    except KeyError:
-        return subtree.ds
+    # Get the analysis time window if present
+    dataset_palmeta = subtree.swarmpal.pal_meta.get(".", {})
+    window = dataset_palmeta.get("analysis_window")
+    # Slice out the relevant part of the dataset
+    if tlims:
+        subset_ds = subtree.ds.sel({"TFA_Time": slice(tlims[0], tlims[1])})
+    elif clip_times and window:
+        subset_ds = subtree.ds.sel({"TFA_Time": slice(window[0], window[1])})
+    else:
+        subset_ds = subtree.ds
+    return subset_ds
 
 
-def time_series(datatree, ax=None, clip_times=True):
-    """Plot the time series of the active variable"""
+def time_series(datatree, ax=None, clip_times=True, tlims=None):
+    """Plot the time series of the active variable
+
+    Parameters
+    ----------
+    datatree : DataTree
+        A datatree from the TFA toolbox
+    ax : AxesSubplot, optional
+        Axes onto which to plot
+    clip_times : bool, optional
+        Clip to the analysis window, by default True
+    tlims : tuple(str, str), optional
+        Tuple of ISO strings to limit the plot to
+
+    Returns
+    -------
+    fig, ax
+    """
     # Extract relevant DataArray (da) and info
     meta = _get_tfa_meta(datatree)
-    ds = _get_active_dataset_window(datatree, meta=meta, clip_times=clip_times)
+    ds = _get_active_dataset_window(
+        datatree, meta=meta, clip_times=clip_times, tlims=tlims
+    )
     da = ds["TFA_Variable"]
     da_origin_name = meta["TFA_Preprocess"]["active_variable"]
     use_magnitude = meta["TFA_Preprocess"]["use_magnitude"]
@@ -54,11 +73,35 @@ def time_series(datatree, ax=None, clip_times=True):
     return fig, ax
 
 
-def spectrum(datatree, log=True, clip_times=True, levels=None, ax=None, **kwargs):
-    """Plot the dynamic spectrum of the result of the wavelet transform"""
+def spectrum(
+    datatree, ax=None, clip_times=True, tlims=None, log=True, levels=None, **kwargs
+):
+    """Plot the dynamic spectrum of the result of the wavelet transform
+
+    Parameters
+    ----------
+    datatree : DataTree
+        A datatree from the TFA toolbox
+    ax : AxesSubplot, optional
+        Axes onto which to plot
+    clip_times : bool, optional
+        Clip to the analysis window, by default True
+    tlims : tuple(str, str), optional
+        Tuple of ISO strings to limit the plot to
+    log : bool, optional
+        Logarithmic scale, by default True
+    levels : ndarray, optional
+        Override the levels used in the colorbar
+
+    Returns
+    -------
+    fig, ax
+    """
     # Extract relevant DataArray (da) and info
     meta = _get_tfa_meta(datatree)
-    ds = _get_active_dataset_window(datatree, meta=meta, clip_times=clip_times)
+    ds = _get_active_dataset_window(
+        datatree, meta=meta, clip_times=clip_times, tlims=tlims
+    )
     da = ds["wavelet_power"]
     # Create new DataArray to be plotted
     if log:
@@ -95,11 +138,13 @@ def spectrum(datatree, log=True, clip_times=True, levels=None, ax=None, **kwargs
     return fig, ax
 
 
-def _get_wave_index(datatree, clip_times=True):
+def _get_wave_index(datatree, clip_times=True, tlims=None):
     """Evaluate wave index from wavelet power"""
     # Extract relevant DataArray (da) and info
     meta = _get_tfa_meta(datatree)
-    ds = _get_active_dataset_window(datatree, meta=meta, clip_times=clip_times)
+    ds = _get_active_dataset_window(
+        datatree, meta=meta, clip_times=clip_times, tlims=tlims
+    )
     da = ds["wavelet_power"]
     return xr.DataArray(
         data=np.nansum(da, axis=0),
@@ -108,9 +153,21 @@ def _get_wave_index(datatree, clip_times=True):
     )
 
 
-def wave_index(datatree, ax=None, clip_times=True):
-    """Plot the index of wave activity"""
-    da = _get_wave_index(datatree, clip_times=clip_times)
+def wave_index(datatree, ax=None, clip_times=True, tlims=None):
+    """Plot the index of wave activity
+
+    Parameters
+    ----------
+    datatree : DataTree
+        A datatree from the TFA toolbox
+    ax : AxesSubplot, optional
+        Axes onto which to plot
+    clip_times : bool, optional
+        Clip to the analysis window, by default True
+    tlims : tuple(str, str), optional
+        Tuple of ISO strings to limit the plot to
+    """
+    da = _get_wave_index(datatree, clip_times=clip_times, tlims=tlims)
     fig, ax = (None, ax) if ax else plt.subplots(1, 1)
     da.plot.line(x="TFA_Time", ax=ax)
     ax.set_xlabel("Time")
