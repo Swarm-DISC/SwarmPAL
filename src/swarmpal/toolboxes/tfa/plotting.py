06862061f0d1773076ee1875e749ed90c260ac8d
Ashley Smith <ashley.smith@magneticearth.org>
2023-04-04 16:26:55 +0100

    Add time clipping and wave_index and quicklooks

--- src/swarmpal/toolboxes/tfa/plotting.py
+++ src/swarmpal/toolboxes/tfa/plotting.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+import xarray as xr
 
 from swarmpal.utils.exceptions import PalError
 
@@ -13,7 +15,7 @@ def _get_tfa_meta(datatree):
     return pal_processes_meta
 
 
-def _get_active_dataset_window(datatree, meta=None):
+def _get_active_dataset_window(datatree, meta=None, clip_times=True):
     """Get the dataset, subselected to the analysis window"""
     pal_processes_meta = meta if meta else _get_tfa_meta(datatree)
     tfa_preprocess_meta = pal_processes_meta.get("TFA_Preprocess")
@@ -21,17 +23,20 @@ def _get_active_dataset_window(datatree, meta=None):
     try:
         dataset_palmeta = subtree.swarmpal.pal_meta["."]
         window = dataset_palmeta["analysis_window"]
-        subset_ds = subtree.ds.sel({"TFA_Time": slice(window[0], window[1])})
+        if clip_times:
+            subset_ds = subtree.ds.sel({"TFA_Time": slice(window[0], window[1])})
+        else:
+            subset_ds = subtree.ds
         return subset_ds
     except KeyError:
         return subtree.ds
 
 
-def time_series(datatree, ax=None):
+def time_series(datatree, ax=None, clip_times=True):
     """Plot the time series of the active variable"""
     # Extract relevant DataArray (da) and info
     meta = _get_tfa_meta(datatree)
-    ds = _get_active_dataset_window(datatree, meta=meta)
+    ds = _get_active_dataset_window(datatree, meta=meta, clip_times=clip_times)
     da = ds["TFA_Variable"]
     da_origin_name = meta["TFA_Preprocess"]["active_variable"]
     use_magnitude = meta["TFA_Preprocess"]["use_magnitude"]
@@ -44,6 +49,7 @@ def time_series(datatree, ax=None):
     ytext = f"TFA: {da_label}"
     ytext = f"{ytext} ({units})" if units else ytext
     ax.set_ylabel(ytext)
+    ax.set_xlabel("Time")
     ax.grid()
     return fig, ax
 
@@ -52,7 +58,7 @@ def spectrum(datatree, log=True, clip_times=True, levels=None, ax=None, **kwargs
     """Plot the dynamic spectrum of the result of the wavelet transform"""
     # Extract relevant DataArray (da) and info
     meta = _get_tfa_meta(datatree)
-    ds = _get_active_dataset_window(datatree, meta=meta)
+    ds = _get_active_dataset_window(datatree, meta=meta, clip_times=clip_times)
     da = ds["wavelet_power"]
     # Create new DataArray to be plotted
     if log:
@@ -84,9 +90,47 @@ def spectrum(datatree, log=True, clip_times=True, levels=None, ax=None, **kwargs
         ax=ax,
         **kwargs,
     )
+    # Adjust axes
+    ax.set_xlabel("Time")
+    return fig, ax
+
+
+def _get_wave_index(datatree, clip_times=True):
+    """Evaluate wave index from wavelet power"""
+    # Extract relevant DataArray (da) and info
+    meta = _get_tfa_meta(datatree)
+    ds = _get_active_dataset_window(datatree, meta=meta, clip_times=clip_times)
+    da = ds["wavelet_power"]
+    return xr.DataArray(
+        data=np.nansum(da, axis=0),
+        coords={"TFA_Time": da["TFA_Time"]},
+        name="wave_index",
+    )
+
+
+def wave_index(datatree, ax=None, clip_times=True):
+    """Plot the index of wave activity"""
+    da = _get_wave_index(datatree, clip_times=clip_times)
+    fig, ax = (None, ax) if ax else plt.subplots(1, 1)
+    da.plot.line(x="TFA_Time", ax=ax)
+    ax.set_xlabel("Time")
+    ax.grid()
     return fig, ax
 
 
 def quicklook(datatree):
     """Returns a figure overviewing relevant contents of the data"""
-    ...
+    fig = plt.figure(figsize=(15, 6))
+    gs = gridspec.GridSpec(nrows=2, ncols=2, width_ratios=[1, 1])
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[1, 0])
+    ax3 = fig.add_subplot(gs[:, 1])
+    time_series(datatree, ax=ax1)
+    try:
+        wave_index(datatree, ax=ax2)
+        spectrum(datatree, ax=ax3)
+    except KeyError:
+        pass
+    ax1.set_xticklabels([])
+    ax1.set_xlabel("")
+    return fig, (ax1, ax2, ax3)
