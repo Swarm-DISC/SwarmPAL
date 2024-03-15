8cb5635506386d16acdc8fcdb8f60dd8a2c50784
Ashley Smith <ashley.smith@magneticearth.org>
2024-03-15 16:36:47 +0000

    Attach auxiliary information in output

--- src/swarmpal/toolboxes/fac/processes.py
+++ src/swarmpal/toolboxes/fac/processes.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import logging
+
 import matplotlib.pyplot as plt
 from datatree import DataTree, register_datatree_accessor
 from numpy import stack
@@ -7,6 +9,8 @@ from xarray import Dataset
 
 from swarmpal.io import PalProcess
 from swarmpal.toolboxes.fac.fac_algorithms import fac_single_sat_algo
+
+logger = logging.getLogger(__name__)
 
 __all__ = (
     "FAC_single_sat",
@@ -28,6 +32,7 @@ class FAC_single_sat(PalProcess):
         measurement_varname: str = "B_NEC",
         inclination_limit: float = 30,
         time_jump_limit: int = 1,
+        include_auxiliaries: bool = True,
     ) -> None:
         """Configures the process
 
@@ -43,6 +48,8 @@ class FAC_single_sat(PalProcess):
             Limit of inclination for FAC validity (in degrees), by default 30
         time_jump_limit : int, optional
             Maximum allowable time step in data for FAC validity (in seconds), by default 1
+        include_auxiliaries : bool, optional
+            Whether to include e.g. Latitude, Longitude, Flags, etc, by default True
         """
         self.config = dict(
             dataset=dataset,
@@ -50,18 +57,19 @@ class FAC_single_sat(PalProcess):
             measurement_varname=measurement_varname,
             inclination_limit=inclination_limit,
             time_jump_limit=time_jump_limit,
+            include_auxiliaries=include_auxiliaries,
         )
 
     def _call(self, datatree):
         # Identify inputs for algorithm
         subtree = datatree[self.config.get("dataset")]
-        dataset = subtree.ds
+        dataset_in = subtree.ds
         # Apply algorithm
         fac_results = fac_single_sat_algo(
-            time=self._get_time(dataset),
-            positions=self._get_positions(dataset),
-            B_res=self._get_B_res(dataset),
-            B_model=self._get_B_model(dataset),
+            time=self._get_time(dataset_in),
+            positions=self._get_positions(dataset_in),
+            B_res=self._get_B_res(dataset_in),
+            B_model=self._get_B_model(dataset_in),
             inclination_limit=self.config.get("inclination_limit"),
             time_jump_limit=self.config.get("time_jump_limit"),
         )
@@ -75,6 +83,8 @@ class FAC_single_sat(PalProcess):
         )
         ds_out["FAC"].attrs = {"units": "uA/m2"}
         ds_out["IRC"].attrs = {"units": "uA/m2"}
+        if self.config.get("include_auxiliaries"):
+            ds_out = self._append_aux(dataset_in, ds_out)
         datatree["PAL_FAC_single_sat"] = DataTree(data=ds_out)
         return datatree
 
@@ -102,6 +112,26 @@ class FAC_single_sat(PalProcess):
     def _get_B_model(self, dataset):
         model_varname = self.config.get("model_varname", "B_NEC_Model")
         return dataset.get(model_varname).data
+
+    def _append_aux(self, ds_in, ds_out):
+        """Extract auxiliary information from inputs and add to output dataset"""
+        aux_in = set(ds_in.data_vars)
+        aux_desired = {
+            "Latitude",
+            "Longitude",
+            "Radius",
+            "Flags_F",
+            "Flags_B",
+            "Flags_q",
+        }
+        aux_matched = aux_desired.intersection(aux_in)
+        aux_missing = aux_desired.difference(aux_in)
+        logging.warning(f"Missing auxiliaries: {aux_missing}")
+        ds_in_interpd = ds_in[list(aux_matched)].interp_like(ds_out, method="nearest")
+        ds_out = ds_out.assign(
+            {aux_name: ds_in_interpd[aux_name] for aux_name in aux_matched}
+        )
+        return ds_out
 
 
 @register_datatree_accessor("swarmpal_fac")
