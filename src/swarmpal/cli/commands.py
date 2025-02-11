a9150b696bd050239eafda21abf40af35e8553fa
Ashley Smith <ashley.smith@ed.ac.uk>
2025-02-11 17:17:10 +0000

    Replace toolboxes.fac.presets with express.(...)

--- src/swarmpal/cli/commands.py
+++ src/swarmpal/cli/commands.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import click
 
-from swarmpal.toolboxes.fac.presets import fac_single_sat as _fac_single_sat
+from swarmpal.express import fac_single_sat as _fac_single_sat
 from swarmpal.utils.configs import SPACECRAFT_TO_MAGLR_DATASET
 from swarmpal.utils.queries import last_available_time as _last_available_time
 
@@ -23,19 +23,19 @@ def spacecraft():
 @click.option(
     "--spacecraft", required=True, help="Check available with: swarmpal spacecraft"
 )
-@click.option("--grade", required=True, help="'OPER' or 'FAST'")
 @click.option("--time_start", required=True, help="ISO 8601 time")
 @click.option("--time_end", required=True, help="ISO 8601 time")
-@click.option("--output", required=True, help="Output CDF file")
+@click.option("--grade", required=True, help="'OPER' or 'FAST'")
+@click.option("--to_cdf_file", required=True, help="Output CDF file")
 def fac_single_sat(
     spacecraft: str,
-    grade: str,
     time_start: str,
     time_end: str,
-    output: str,
+    grade: str,
+    to_cdf_file: str
 ):
     """Execute FAC single-satellite processor"""
-    return _fac_single_sat(spacecraft, grade, time_start, time_end, output)
+    return _fac_single_sat(spacecraft, time_start, time_end, grade, to_cdf_file)
 
 
 @cli.command(add_help_option=True)
