a3deb72fba2c653a6bff86ad5a47c02846095729
Ashley Smith <ashley.smith@ed.ac.uk>
2025-04-30 16:25:06 +0100

    Rename Cryosat to CryoSat-2

--- src/swarmpal/utils/configs.py
+++ src/swarmpal/utils/configs.py
@@ -5,7 +5,7 @@ SPACECRAFT_TO_MAGLR_DATASET = {
     "Swarm-B": "SW_OPER_MAGB_LR_1B",
     "Swarm-C": "SW_OPER_MAGC_LR_1B",
     "CHAMP": "CH_ME_MAG_LR_3",
-    "Cryosat": "CS_OPER_MAG",
+    "CryoSat-2": "CS_OPER_MAG",
     "GRACE-A": "GRACE_A_MAG",
     "GRACE-B": "GRACE_B_MAG",
     "GRACE-FO-1": "GF1_OPER_FGM_ACAL_CORR",
