0840bf2b5717cef972c6eba4a24eefd6063e8b7f
Ashley Smith <ashley.smith@ed.ac.uk>
2026-05-01 17:37:29 +0100

    Enable CI for Python 3.10 and 3.14. cleanup

--- noxfile.py
+++ noxfile.py
@@ -14,7 +14,7 @@ def lint(session: nox.Session) -> None:
     session.run("pre-commit", "run", "--all-files", *session.posargs)
 
 
-@nox.session(python=["3.12", "3.13", "3.14"])
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session: nox.Session) -> None:
     """
     Run the unit and regular tests.
